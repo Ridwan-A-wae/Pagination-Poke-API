83a27c2017197de428716a46109eece0578691ad
Ridwan <129216149+Ridwan-A-wae@users.noreply.github.com>
2023-08-14 13:15:57 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,78 @@
-import { useState,useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
-import Pagination from './Pagination'
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Pagination from "./Pagination";
 
 function App() {
-  
-  const [pokemon, setPokemon] = useState([])
+  const [pokemon, setPokemon] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postPerPage, setPostPerPage] = useState(8)
-  
+  const [postPerPage, setPostPerPage] = useState(8);
+  const [load, setLoad] = useState(false);
+
   const lastPostIndex = currentPage * postPerPage;
   const firstPostIndex = lastPostIndex - postPerPage;
-  const currentPost = pokemon.slice(firstPostIndex, lastPostIndex)
+  const currentPost = pokemon.slice(firstPostIndex, lastPostIndex);
 
-  const fetchData = async() => {
+  const fetchData = async () => {
     const pokemonPromise = [];
 
-    for(let i = 1; i<=100; i++) {
-      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    setLoad(true);
+
+    for (let i = 1; i <= 100; i++) {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
       pokemonPromise.push(res);
     }
 
     try {
-      const results = await Promise.all(pokemonPromise)
-      const data = results.map((response) => response.data )
-      setPokemon(data)
-    }catch (error) {
-      console.error(error)
+      const results = await Promise.all(pokemonPromise);
+      const data = results.map((response) => response.data);
+      setPokemon(data);
+    } catch (error) {
+      console.error(error);
     }
-  }
-  pokemon.map((poke) => {
-    console.log(poke)
-  })
+    setLoad(false);
+  };
 
   useEffect(() => {
-    fetchData()
-  },[])
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <div className='headCon'>
-      <h1 className='head'>Pokemon Card</h1>
-      <p className='created'>Created by Ridwan</p>
+    <>
+      <div className="headCon">
+        <h1 className="head">Pokemon Card</h1>
+        <p className="created">Created by Ridwan</p>
       </div>
       {/* Pokemon */}
-      <div className='cardCon'>
-        {pokemon ? currentPost.map((poke) => (
-          //Card
-          <div className='card'key={poke.name}> 
-
-          <h1 className='name' >{poke.name} </h1>
-
-          <img className='img' src={poke?.sprites.other.home.front_default} alt={poke.name} />
-
+      {load ? (
+        <h1 style={{color:"white",paddingTop:"20rem"}}>Loading . . .</h1>
+      ) : (
+        <>
+          <div className="cardCon">
+            {pokemon
+              ? currentPost.map((poke) => (
+                  //Card
+                  <div className="card" key={poke.name}>
+                    <h1 className="name">{poke.name}</h1>
+                    <img
+                      className="img"
+                      src={poke?.sprites.other.home.front_default}
+                      alt={poke.name}
+                    />
+                  </div>
+                ))
+              : null}
           </div>
-        )) :null}
-      </div>
-      <Pagination 
-      totalPosts = {pokemon.length} 
-      postsPerPage = {postPerPage}
-      setCurrentPage={setCurrentPage}
-      currentPage={currentPage}
-      />
-    </div>
-  )
+          <Pagination
+            totalPosts={pokemon.length}
+            postsPerPage={postPerPage}
+            setCurrentPage={setCurrentPage}
+            currentPage={currentPage}
+          />
+        </>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
